53230a40eafa78db36fd4fa03d6d73977dc6d5d5
amit sonagra <97925980+amitusf@users.noreply.github.com>
2022-05-03 14:57:58 -0400

    Cleaned repo

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,189 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.DataAccess;
+using WebApplication1.Models;
+using System.Data;
+using Newtonsoft.Json;
+using System.Web;
+using System.Net;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly UniversityDbContext _context;
+
+        public HomeController(UniversityDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+
+        
+        public IActionResult FrontPage()
+        {
+
+           
+
+            return View();
+        }
+
+        public IActionResult Content()
+        {
+            return View();
+        }
+
+            // GET: Students1
+            public async Task<IActionResult> Index()
+        {
+            var dataPoints = (from i in _context.Student
+                              select new DataPoints{ x = i.studentId, y = Int32.Parse(i.studentScore) });
+
+            string json = JsonConvert.SerializeObject(dataPoints.ToArray());
+
+            ViewBag.DataPoints = json;
+
+            return View(await _context.Student.ToListAsync());
+        }
+
+        // GET: Students1/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Student
+                .FirstOrDefaultAsync(m => m.studentId == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+
+
+            return View(student);
+        }
+
+        // GET: Students1/Create
+        public IActionResult Create()
+        {
+
+            return View();
+
+        }
+
+        // POST: Students1/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("studentId,studentName,studentScore,appliedUniversity")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+        // GET: Students1/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Student.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students1/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("studentId,studentName,studentScore,appliedUniversity")] Student student)
+        {
+            if (id != student.studentId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(student);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StudentExists(student.studentId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+        // GET: Students1/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Student
+                .FirstOrDefaultAsync(m => m.studentId == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        // POST: Students1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var student = await _context.Student.FindAsync(id);
+            _context.Student.Remove(student);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StudentExists(int id)
+        {
+            return _context.Student.Any(e => e.studentId == id);
+        }
+    }
+}
---
+++ DataAccess/UniversityDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.DataAccess
+{
+    public class UniversityDbContext : DbContext
+    {
+        public UniversityDbContext(DbContextOptions<UniversityDbContext> options) : base(options) { }
+
+        public DbSet<Campus> Campus { get; set; } = null!;
+        public DbSet<Department> Department { get; set; } = null!;
+        public DbSet<Student> Student { get; set; } = null!;
+
+        
+        
+
+
+    }
+}
